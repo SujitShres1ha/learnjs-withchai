093c18be868fffdadfcd85e7711d0ed7e465a994
Sujit <sujit@Sujits-MacBook-Air.local>
2024-01-30 09:47:21 +0545

    Second Day

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -10,3 +10,18 @@ const id_2 = Symbol(123)
 console.log(id === id_2)
 
 // Non-primitive(Reference) - array, object, function 
+
+// Stack(primitive) vs Heap(non-primitive)
+
+let a = 1
+let b = a
+b = 2
+console.log(a,b)
+
+
+let c = {
+    name: "Sujit"
+}
+let d = c
+d.name = "Nobel"
+console.log(c.name,d.name)
---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+let a = " Sujit "
+let b = new String("Sujit")
+
+console.log(a.slice(-5,5))
+
+console.log(a.trim(),a)
+
+console.log(typeof(a),typeof(b))
+
+console.log(`Hello my name is ${a.trim()}`) 
